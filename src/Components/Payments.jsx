c361e2af3256b2b37a3c2232ba4bc4f5403c6f69
thiyari <manikanth578@gmail.com>
2026-05-08 19:30:41 +0530

    Payments page update

--- src/Components/Payments.jsx
+++ src/Components/Payments.jsx
@@ -1,9 +1,223 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Payments = () => {
+
+  const options = [
+    { value: 'Left ($100.00)', label: 'Left ($100.00)'},
+    { value: 'Option2', label: 'Option2'},
+    { value: 'Option3', label: 'Option3'}
+  ];
+
+  const paymentOptions = [
+    { value: 'Payment', label: 'Payment'},
+    { value: 'Option2', label: 'Option2'},
+    { value: 'Option3', label: 'Option3'}
+  ];
+  const [selected, setSelected] = useState("");
+  const [paymentSelected, setPaymentSelected] = useState("");
+
+  const handleChangeSelect = (event) => {
+    setSelected(event.target.value);
+  };
+  const handleChangePaymentSelect = (event) => {
+    setPaymentSelected(event.target.value);
+  };
   return (
-    <div>
-      Payments page
+    <div className="container">
+          
+            <div className="row">
+                <h5 className="mt-4" align="left"><b>Payment Settings</b></h5>
+            </div>
+
+            <div className="row bg-light border" align="left">
+                <p className="m-2"><i className="fa-solid fa-circle-info fa-lg"></i>
+                &nbsp; Here you find all the payment related settings.</p>
+            </div>
+
+            <form>
+              <div className="form-group row mt-4" align="left">
+                <label htmlFor="currencySymbol" className="col-sm-3 col-form-label"><b>Currency Symbol</b></label>
+                <div className="col-sm-4">
+                    <input 
+                        type="text"  
+                        className="form-control mb-2" 
+                        id="" 
+                        placeholder=""
+                        name=""
+                        value=""
+                        />
+                </div>
+                <div className="col-sm-5"></div>
+              </div>
+
+              <div className="form-group row mt-4" align="left">
+                <label htmlFor="currencyPosition" className="col-sm-3 col-form-label"><b>Currency Position</b></label>
+                <div className="col-sm-4">
+                    <div className="d-flex justify-content-center">
+                      <select className="form-select" value={selected} onChange={handleChangeSelect}>
+                        {options.map((option, index) => (
+                            <option value={option.value} key={index}>{option.label}</option>
+                          ))}
+                      </select>
+                    </div>
+                </div>
+                <div className="col-sm-5"></div>
+              </div>
+
+
+              <div className="form-group row mt-4" align="left">
+                <label htmlFor="thousandSeperator" className="col-sm-3 col-form-label"><b>Thousand Seperator</b></label>
+                <div className="col-sm-4">
+                    <input 
+                        type="text"  
+                        className="form-control mb-2" 
+                        id="" 
+                        placeholder=""
+                        name=""
+                        value=""
+                        />
+                </div>
+                <div className="col-sm-5"></div>
+              </div>
+
+
+
+              <div className="form-group row mt-4" align="left">
+                <label htmlFor="decimalSeperator" className="col-sm-3 col-form-label"><b>Decimal Seperator</b></label>
+                <div className="col-sm-4">
+                    <input 
+                        type="text"  
+                        className="form-control mb-2" 
+                        id="" 
+                        placeholder=""
+                        name=""
+                        value=""
+                        />
+                </div>
+                <div className="col-sm-5"></div>
+              </div>
+
+
+
+              <div className="form-group row mt-4" align="left">
+                <label htmlFor="numberOfDecimals" className="col-sm-3 col-form-label"><b>Number of Decimals</b></label>
+                <div className="col-sm-4">
+                    <input 
+                        type="text"  
+                        className="form-control mb-2" 
+                        id="" 
+                        placeholder=""
+                        name=""
+                        value=""
+                        />
+                </div>
+                <div className="col-sm-5"></div>
+              </div>
+
+
+
+              <div className="form-group row mt-4" align="left">
+                <label htmlFor="currencyPosition" className="col-sm-3 col-form-label"><b>Payment Page</b></label>
+                <div className="col-sm-4">
+                    <div className="d-flex justify-content-center">
+                      <select className="form-select" value={selected} onChange={handleChangePaymentSelect}>
+                        {paymentOptions.map((option, index) => (
+                            <option value={option.value} key={index}>{option.label}</option>
+                          ))}
+                      </select>
+                    </div>
+                </div>
+                <div className="col-sm-5"></div>
+                <div className='col-sm-3'></div>
+                <div className='col-sm-9'>
+                    <label className="form-label text-muted mt-2" style={{fontSize: "0.6rem"}}><i>Choose a page to use for PayPal and other <a href="#">available payment gateway</a> messages and other confirmations.<br></br>A blank page named Payment would be perfect.</i></label>
+                </div> 
+              </div>
+
+
+
+              <div className="form-group row mt-2" align="left">
+                <label htmlFor="paymentPageFooter" className="col-sm-3 col-form-label"><b>Payment Page Footer</b></label>
+                <div className="col-sm-6">
+                  <textarea 
+                      className="form-control" 
+                      id="" 
+                      placeholder=""
+                      name=""
+                      rows="4"
+                      onChange=""
+                      ></textarea>
+                  <label className="form-label text-muted mt-2" style={{fontSize: "0.6rem"}}><i>The footer will be displayed at the bottom of the payment page. Basic HTML is allowed.
+                    <br></br>Use this to provide additional payment instructions, if desired.</i></label>
+                </div>
+                <div className="col-sm-3"></div>
+              </div>
+
+              <hr></hr>
+
+              <div className="row">
+                  <h5 align="left"><b>Payment Methods</b></h5>
+              </div>
+
+              <div className="form-group row mt-2" align="left">
+                <div className='col-sm-3'>
+                <label htmlFor="bank"><b>Bank</b></label>
+                <br></br>
+                <label className="form-label text-muted mt-2" style={{fontSize: "0.6rem"}}><i>Displayed on the invoice.</i></label>
+              </div>
+                <div className="col-sm-6">
+                  <textarea 
+                      className="form-control" 
+                      id="" 
+                      placeholder=""
+                      name=""
+                      rows="4"
+                      onChange=""
+                      ></textarea>
+                  <label className="form-label text-muted mt-2" style={{fontSize: "0.6rem"}}><i>Add your bank account details if you wish to allow direct bank deposits. HTML is allowed.</i></label>
+                </div>
+                <div className="col-sm-3"></div>
+              </div>
+
+
+
+              <div className="form-group row mt-2" align="left">
+                <div className='col-sm-3'>
+                <label htmlFor="genericPayment"><b>Generic Payment</b></label>
+                <br></br>
+                <label className="form-label text-muted mt-2" style={{fontSize: "0.6rem"}}><i>Displayed on the invoice.</i></label>
+              </div>
+                <div className="col-sm-6">
+                  <textarea 
+                      className="form-control" 
+                      id="" 
+                      placeholder=""
+                      name=""
+                      rows="4"
+                      onChange=""
+                      ></textarea>
+                  <label className="form-label text-muted mt-2" style={{fontSize: "0.6rem"}}><i>Set a generic message or include further instructions for the user on how to pay. HTML is allowed.</i></label>
+                </div>
+                <div className="col-sm-3"></div>
+              </div>
+
+
+            <div className="row bg-light border" align="left">
+                <p className="m-2">
+                PayPal Gateway</p>
+            </div>
+
+
+              <div className="form-group row mt-4" align="left">
+                <div className="col-sm-3">
+                  <button type="submit" className="btn btn-primary">Save</button>
+                </div>
+                <div className="col-sm-4"></div>
+                <div className="col-sm-3"></div>
+              </div>
+
+
+            </form>
     </div>
   )
 }
