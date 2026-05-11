0f3e83df4e1d82189bb3364d546b9b6e7cb1e32d
thiyari <manikanth578@gmail.com>
2026-05-11 19:38:07 +0530

    update template

--- src/Components/Templates/Template1.jsx
+++ src/Components/Templates/Template1.jsx
@@ -2,13 +2,34 @@ import React from 'react'
 import { useFormContext } from "../../context/FormContext";
 
 const Template1 = () => {
+
   const { formData } = useFormContext();
-    
+  
+  const future_date = (days) => {
+    // Today's date
+    const today = new Date();
+    // Add 14 days
+    const futureDate = new Date(today);
+    futureDate.setDate(today.getDate() + days);
+
+    return futureDate.toLocaleDateString('en-GB');
+  }
+
+  const invoice_number = (prefix, number, suffix) => {
+    if (prefix != "" && suffix == "") {
+        return <div>{prefix}-{number}</div>;
+    } else if (suffix != "" && prefix == "") {
+        return <div>{number}-{suffix}</div>;
+    } else {
+        return <div></div>
+    }
+  }
+
   return (
     <div className='mt-4 container' 
     style={{
       margin: 0,
-      border: "2px solid blue",
+      border: "2px solid darkblue",
       minHeight: "100vh",
       boxSizing: "border-box"
     }}>
@@ -29,10 +50,39 @@ const Template1 = () => {
         </div>
             
         <div className='row mt-2'>
-            <div className='col-sm-3' style={{backgroundColor: 'darkblue', color: 'white', textAlign:'left'}}>
-                Address
+            <div className='col-sm-5' style={{padding: "10px 10px 10px 30px", backgroundColor: 'darkblue', color: 'white', textAlign:'left'}}>
+                <b>From:</b><br></br>
+                <p style={{fontFamily: "Calibri"}}>
+                Ultrakey IT Solutions Private Limited<br></br>
+                Flat No. 204, 2nd Floor, Cyber Residency,<br></br>
+                Indira Nagar, Gachibowli,<br></br>
+                Hyderbad, Telangana, India-500032<br></br>
+                support@ultrakeyit.com<br></br>
+                GST No: 36AADCU5062A1ZO</p>
             </div>
-            <div className='col-sm-9'></div>
+            <div className='col-sm-2'></div>
+            <div className='col-sm-5'>
+                <div className='row' style={{marginRight: "10px"}}>
+                    <div className='row'>
+                        <div className='col-sm-7' style={{textAlign: "right", padding: "3px 10px 3px 0px"}}><p>Invoice Number</p></div>
+                        <div className='col-sm-5' style={{textAlign: "right"}}>
+                            <p>{invoice_number(formData.invoices.prefix, formData.invoices.nextNumber, formData.invoices.suffix)}</p>   
+                        </div>
+                    </div>
+                    <div className='row'>
+                        <div className='col-sm-7' style={{textAlign: "right", padding: "3px 10px 3px 0px"}}><p>Invoice Date</p></div>
+                        <div className='col-sm-5'style={{textAlign: "right"}}><p>{new Date().toLocaleDateString('en-GB')}</p></div>
+                    </div>
+                    <div className='row' >
+                        <div className='col-sm-7' style={{textAlign: "right", padding: "3px 10px 3px 0px"}}><p>Due Date</p></div>
+                        <div className='col-sm-5'style={{textAlign: "right"}}><p>{future_date(formData.invoices.dueDate)}</p></div>
+                    </div>
+                    <div className='row' style={{backgroundColor: "darkblue", padding: "10px"}}>
+                        <div className='col-sm-7' style={{textAlign: "right"}}><p style={{color: "white", fontWeight: "bold"}}>TOTAL DUE</p></div>
+                        <div className='col-sm-5'style={{textAlign: "right"}}><p style={{color: "white", fontWeight: "bold"}}>due date</p></div>
+                    </div>
+                </div>
+            </div>
         </div>
         </div>
   )
