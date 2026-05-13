24071586a02f3291eb3efde1d039e01b6aa657e3
thiyari <manikanth578@gmail.com>
2026-05-13 19:54:18 +0530

    pdf download test

--- src/Components/PDF.jsx
+++ src/Components/PDF.jsx
@@ -1,17 +1,46 @@
-import React from 'react'
+import React, { useRef } from "react";
+import html2pdf from "html2pdf.js";
 import { useFormContext } from "../context/FormContext";
 import Template1 from "./Templates/Template1";
 import Template2 from "./Templates/Template2";
 
 const PDF = () => {
   const { formData } = useFormContext();
+  const contentRef = useRef();
+
+  const downloadPDF = () => {
+    const element = contentRef.current;
+
+    const options = {
+      margin: 0.5,
+      filename: "document.pdf",
+      image: { type: "jpeg", quality: 0.98 },
+      html2canvas: {
+        scale: 2,
+        useCORS: true,
+      },
+      jsPDF: {
+        unit: "in",
+        format: "a4",
+        orientation: "portrait",
+      },
+    };
+
+    html2pdf().set(options).from(element).save();
+  };
 
   return (
     <div>
-      {formData.invoices.template === "template1" && <Template1/>}
-      {formData.invoices.template === "template2" && <Template2/>}
-    </div>
-  )
-}
+      {/* PDF CONTENT */}
+      <div ref={contentRef} style={{ padding: 0 }}>
+        {formData.invoices.template === "template1" && <Template1 />}
+        {formData.invoices.template === "template2" && <Template2 />}
+      </div>
 
-export default PDF
+      {/* BUTTON OUTSIDE PDF CONTENT */}
+      <button onClick={downloadPDF}>Download PDF</button>
+    </div>
+  );
+};
+
+export default PDF;
