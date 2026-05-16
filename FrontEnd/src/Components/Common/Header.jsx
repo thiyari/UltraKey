ab3bc26d23f713ac74bb2ebe1ef8251664cfe950
thiyari <manikanth578@gmail.com>
2026-05-16 11:54:49 +0530

    update header with Link instead of anchor

--- FrontEnd/src/Components/Common/Header.jsx
+++ FrontEnd/src/Components/Common/Header.jsx
@@ -1,43 +1,44 @@
 import React from 'react'
 import './Common.css'
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light border">
       <div className="container">
-        <a className="navbar-brand" href="/"></a>
+        <Link className="navbar-brand" to="/"></Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link"  href="/">General</a>
+              <Link className="nav-link" to="/">General</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/business">Business</a>
+              <Link className="nav-link" to="/business">Business</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/quotes">Quotes</a>
+              <Link className="nav-link" to="/quotes">Quotes</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/invoices">Invoices</a>
+              <Link className="nav-link" to="/invoices">Invoices</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/payments">Payments</a>
+              <Link className="nav-link" to="/payments">Payments</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/tax">Tax</a>
+              <Link className="nav-link" to="/tax">Tax</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/emails">Emails</a>
+              <Link className="nav-link" to="/emails">Emails</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/pdf">PDF</a>
+              <Link className="nav-link" to="/pdf">PDF</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/translate">Translate</a>
+              <Link className="nav-link" to="/translate">Translate</Link>
             </li>
           </ul>
         </div>
