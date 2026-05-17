6214db1f49bae57e9902171964dd52cd6e9d503d
thiyari <manikanth578@gmail.com>
2026-05-18 00:21:13 +0530

    update invoice settings

--- BackEnd/src/models/settings.model.js
+++ BackEnd/src/models/settings.model.js
@@ -61,7 +61,61 @@ const settingsSchema = new mongoose.Schema(
       type: String,
       required: false,
     }
-  },    
+  },
+  invoices: {
+    key: {
+      type: String,
+      required: false,
+    },
+    prefix: {
+      type: String,
+      required: false,
+    },
+    suffix: {
+      type: String,
+      required: false,
+    },
+    autoIncrement: {
+      type: String,
+      required: false,
+    },
+    nextNumber: {
+      type: String,
+      required: false,
+    },
+    dueDate: {
+      type: String,
+      required: false,
+    },
+    hideAdjustField: {
+      type: String,
+      required: false,
+    },
+    termsAndConditions: {
+      type: String,
+      required: false,
+    },
+    footer: {
+      type: String,
+      required: false,
+    },
+    notices: {
+      type: String,
+      required: false,
+    },
+    template: {
+      type: String,
+      required: false,
+    },
+    customCSS: {
+      type: String,
+      required: false,
+    },
+    toAddress: {
+      type: String,
+      required: false,
+    }
+  },      
   },  
   {
     timestamps: true,
